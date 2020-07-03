fc0a2c45b099c78e273ea5811c7ba3bd1da72bc3
C0D3R <jefmonva@gmail.com>
2020-07-03 13:03:35 -0500

    primer commit servidor

---
+++ controllers/usuarioController.js
@@ -0,0 +1,59 @@
+const Usuario = require("../models/Usuario");
+const bcryptjs = require("bcryptjs");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
+exports.crearUsuario = async (req, res) => {
+  // Revisar errores
+  const errores = validationResult(req);
+  if (!errores.isEmpty()) {
+    return res.status(400).json({ errores: errores.array() });
+  }
+
+  // Destructuring de email y password
+  const { email, password } = req.body;
+
+  try {
+    //   Revisar que el usuario registrado sea unico
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({ msg: "El usuario ya existe" });
+    }
+
+    // Crear el nuevo usuario
+    usuario = new Usuario(req.body);
+
+    // Hashear el password
+    const salt = await bcryptjs.genSalt(10);
+    usuario.password = await bcryptjs.hash(password, salt);
+
+    // Guardar usuario
+    await usuario.save();
+
+    // Crear y firmar el JWT
+    const payload = {
+      usuario: {
+        id: usuario.id
+      }
+    };
+
+    // Firmar el jwt
+    jwt.sign(
+      payload,
+      process.env.SECRETA,
+      {
+        expiresIn: 3600,
+      },
+      (error, token) => {
+        if (error) throw error;
+
+        // Mensaje de confirmación
+        res.json({ token });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(400).send("Se presento un error");
+  }
+};
---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+  // Leer el token del header
+  const token = req.header("x-auth-token");
+
+  // Revisar si hay token
+  if (!token) {
+    return res
+      .status(401)
+      .json({ msg: "No hay permisos validos por el token" });
+  }
+
+  // Validar el token
+    try {
+        const cifrado = jwt.verify(token, process.env.SECRETA);
+        req.usuario = cifrado.usuario;
+        next();
+  } catch (error) {
+    res.status(401).json({ msg: "Token no válido" });
+  }
+};
